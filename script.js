c6d5a1cdc98fc3238e3d58be694877aca27b7cd0
Nathan Lyons <159852641+Natkiio@users.noreply.github.com>
2024-08-01 13:12:08 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
